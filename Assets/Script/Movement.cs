b1dc022c897556e10bcbcacfab556f0b12445673
Loris Arrigo <loris.arrigo@gmail.com>
2026-04-24 16:59:15 +0100

    First Push
    - inserito progetto;
    - implementato movement (manca lo sprint)

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    InputMap inputs;
+
+    Rigidbody2D rb;
+
+    //Movement
+    [SerializeField] float speed;
+
+    //Jump
+    [SerializeField] LayerMask ground;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float radius;
+    [SerializeField] float jumpForce;
+    bool IsGrounded = false;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputs = new InputMap();
+    }
+
+    private void OnEnable()
+    {
+        inputs.Enable();
+        inputs.Player.Jump.started += JumpAction;
+    }
+    private void OnDisable()
+    {
+        inputs.Disable();
+        inputs.Player.Jump.started -= JumpAction;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 move = inputs.Player.Movement.ReadValue<Vector2>();
+        if (move != Vector2.zero)
+        {
+            Vector2 position = rb.position + Time.fixedDeltaTime * speed * move;
+            rb.position = position;
+        }
+
+        IsGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, ground);
+    }
+    void JumpAction(InputAction.CallbackContext context)
+    {
+        if (IsGrounded)
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, radius);
+    }
+}
