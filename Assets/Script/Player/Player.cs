9ba71e1b58d1374f28e31c3063023ba7ab0ffb37
Loris Arrigo <loris.arrigo@gmail.com>
2026-05-05 12:48:48 +0100

    Animations 1
    - aggiunto sprite di salto;
    - aggiunte animationclips per il Player:
       - idle (frame in loop, questo necessario per far sì che l'animator abbia uno stato di default);
       - Walking (usata per la Walking animation - 60 frame - e per la corsa - 60 frame x 2);
       - Jump (frame trigger, usato in Idle - 60 frame x 0,5-, per la camminata - 60 x 0,75- e per la corsa - 60 x 0,55);
       - Hit (quando vieni colpito flash il Player tra 2 sprite);
       - Death (quando l'ultimo colpo viene preso, il Player diventa immobile e muore. una volta finita l'animazione appare a schermo lo schermo di GameOver);

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,26 +1,28 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour, IDamageable
 {
     InputMap inputs;
-
+    Animator anim;
     Rigidbody2D rb;
 
     //Movement
     [SerializeField] float baseSpeed, sprintSpeed, currentSpeed; //speed variables
-
+    [SerializeField] SpriteRenderer plSprite;
     //Jump
     [SerializeField] LayerMask ground;
     [SerializeField] Transform groundCheck;
     [SerializeField] float jumpForce;
     [SerializeField] float radius; //range of the GroundCheck
-    bool IsGrounded = false;
+    public bool IsGrounded = false;
 
     //health
     [SerializeField] float maxHealth;
     public float currentHealth;
+    [SerializeField] float deathDuration; //hit + death animation
     public static event Action OnHit, OnGO;
 
     public static Player Instance;
@@ -34,55 +36,84 @@ public class Player : MonoBehaviour, IDamageable
         Instance = this;
 
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
         inputs = new InputMap();
     }
 
     private void Start()
     {
         currentHealth = maxHealth;
+        currentSpeed = baseSpeed;
     }
     private void OnEnable()
     {
         inputs.Enable();
         inputs.Player.Jump.started += JumpAction;
+        inputs.Player.Sprint.started += Sprinting;
+        inputs.Player.Sprint.canceled += EndSprinting;
     }
     private void OnDisable()
     {
         inputs.Disable();
         inputs.Player.Jump.started -= JumpAction;
+        inputs.Player.Sprint.started -= Sprinting;
+        inputs.Player.Sprint.canceled -= EndSprinting;
     }
     #region movement
     private void FixedUpdate()
     {
+        IsGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, ground);
         Vector2 move = inputs.Player.Movement.ReadValue<Vector2>();
-        if (inputs.Player.Sprint.IsPressed())
-            currentSpeed = sprintSpeed;
-        else
-            currentSpeed = baseSpeed;
         if (move != Vector2.zero)
         {
+            //player flipping
+            if (move.x < 0)
+                plSprite.flipX = true;
+            else if (move.x > 0)
+                plSprite.flipX = false;
+
+            //player movement
             Vector2 position = rb.position + Time.fixedDeltaTime * currentSpeed * move;
             rb.position = position;
+            if (IsGrounded)
+                anim.SetBool("IsWalking", true);
         }
-
-        IsGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, ground);
+        else if (move == Vector2.zero)
+            anim.SetBool("IsWalking", false);
+    }
+    void Sprinting(InputAction.CallbackContext context)
+    {
+        currentSpeed = sprintSpeed;
+        if (IsGrounded)
+            anim.SetBool("IsRunning", true);
+    }
+    void EndSprinting(InputAction.CallbackContext context)
+    {
+        currentSpeed = baseSpeed;
+        anim.SetBool("IsRunning", false);
     }
     void JumpAction(InputAction.CallbackContext context)
     {
         if (IsGrounded)
+        {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetTrigger("IsJumping");
+        }
     }
     #endregion
     #region Health System
     public void TakeDamage(float dmg)
     {
         currentHealth -= dmg;
+        if(currentHealth > 0)
+            anim.SetTrigger("IsHitted");
         OnHit?.Invoke();
     }
 
     public void Despawn()
     {
-        OnGO?.Invoke();
+        StartCoroutine(DeathSequence());
     }
     #endregion
     void OnDrawGizmos()
@@ -90,5 +121,13 @@ public class Player : MonoBehaviour, IDamageable
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(groundCheck.position, radius);
     }
+    private IEnumerator DeathSequence()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        currentSpeed = 0;
 
+        anim.SetTrigger("IsDying");
+        yield return new WaitForSeconds(deathDuration);
+        OnGO?.Invoke();
+    }
 }
